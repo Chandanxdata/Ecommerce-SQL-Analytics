e3fddb5c7bed8eedbd24a49387473c3dfada36f1
Chandanxdata <pandey4uv@gmail.com>
2025-12-30 10:51:30 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,28 +1,35 @@
+-- Database: ecommerce_db
+-- Purpose: E-commerce analytics schema with relational tables
+
+
+-- Customers table
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(100),
-    email VARCHAR(100),
-    city VARCHAR(50),
-    signup_date DATE
+    customer_city VARCHAR(50)
 );
 
+
+-- Products table
 CREATE TABLE products (
     product_id INT PRIMARY KEY,
-    product_name VARCHAR(100),
     category VARCHAR(50),
-    price DECIMAL(10,2)
+    unit_price INT
 );
 
+
+-- Orders table
 CREATE TABLE orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    order_status VARCHAR(30),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
+
+-- Order Items table
 CREATE TABLE order_items (
-    order_item_id INT PRIMARY KEY,
+    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     product_id INT,
     quantity INT,
@@ -30,11 +37,13 @@ CREATE TABLE order_items (
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
+
+-- Payments table
 CREATE TABLE payments (
-    payment_id INT PRIMARY KEY,
+    payment_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
-    payment_method VARCHAR(30),
-    payment_status VARCHAR(30),
-    amount DECIMAL(10,2),
+    payment_method VARCHAR(50),
+    payment_status VARCHAR(20),
+    amount INT,
     FOREIGN KEY (order_id) REFERENCES orders(order_id)
 );
